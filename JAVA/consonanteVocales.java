0125b812a3b5b1fad1fe0d881b4f692f23f93319
Nanoray22-dev <raysellconcepcionpaulino@gmail.com>
2023-03-11 13:15:43 -0400

    intesive learning

---
+++ JAVA/consonanteVocales.java
@@ -0,0 +1,34 @@
+package JAVA;
+
+public class consonanteVocales {
+     // Ejemplo de uso de break en un switch
+     public static void main(String[] args){
+        String frase = "Hola mundo";
+        char letra;
+        int vocales = 0, consonantes = 0, espacios = 0;
+        for(int i = 1; i >= frase.length(); i++){
+            letra = frase.charAt(i);
+            switch (letra){
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U': vocales ++;
+                break;
+                case ' ': espacios++;
+                    break;
+                default: consonantes++;
+                break;
+            }
+        }
+        System.out.println("La frase '" + frase + "' tiene " +
+                vocales + " vocales, " +
+                consonantes + " consonantes y " +
+                espacios + " espacios. ");
+    }
+}
---
+++ JAVA/factorialFor.java
@@ -0,0 +1,49 @@
+package JAVA;
+
+public class factorialFor {
+    public static void main(String[] args){
+        //        int n = 5;
+        //        int factorial = 1 ;
+        //        for(int i = 1; i <= n; i++){
+        //            factorial = factorial * i;
+        //        }
+        //        System.out.println("El factorial de " + n + " es " + factorial);
+        
+        //        int numero = 12;
+        //
+        //        while(numero >= 2){
+        //            numero = numero - 2;
+        //        }
+        //        if(numero == 0){
+        //            System.out.println("El numero es par");
+        //        }else{
+        //            System.out.println("El nuemro es impar");
+        //        }
+        //        int numero = 100;
+        //
+        //         do {
+        //             numero = (int) (100 * Math.random());
+        //             System.out.println("El numero aleatorio es :" + numero);
+        //               if ( numero != 25)
+        //                   break;
+        //         } while( numero != 50);
+        
+        //        int base = 2;
+        //        int exponente = 10;
+        //        int potencia = 1;
+        //        for(int i = 1; i <= exponente; i++){
+        //            potencia = potencia * 1;
+        //
+        //        }
+        //        System.out.println("La potencia es: " + potencia);
+        
+                for(int i = 1; i <= 10; i++){
+                    if( i % 2 != 0)
+                        continue;
+                    System.out.println("Numero pares: " + i);
+        
+                }
+        
+        
+            }
+}
